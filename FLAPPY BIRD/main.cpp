b00f678878197b843cebe80662c96518ffeba24f
NguyenHongHoang10 <24020139@vnu.edu.vn>
2025-03-22 08:06:09 +0700

    Update main.cpp

--- FLAPPY BIRD/main.cpp
+++ FLAPPY BIRD/main.cpp
@@ -11,6 +11,25 @@
 
 int main(int argc, char* args[])
 {
+    srand(time(NULL));
+    if(initSDL()!=0) return 1;
+
+    SDL_Window* window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if(window == nullptr)
+    {
+        logError(cout, "CreateWindow");
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if(renderer == nullptr)
+    {
+        logError(cout, "CreateRenderer");
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
 
     return 0;
 }
